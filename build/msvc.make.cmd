10b035e89faacf882019d84efe5dca43f4f461fd
Grigore Stefan <g_stefan@yahoo.com>
2021-03-15 12:37:44 +0200

    project-structure

--- build/msvc.make.cmd
+++ build/msvc.make.cmd
@@ -18,5 +18,5 @@ echo "Error: %ACTION%"
 exit 1
 :StepX
 
-call :cmdX xyo-cc --mode=%ACTION% @util/quantum-script-extension-socket.static.compile
-call :cmdX xyo-cc --mode=%ACTION% @util/quantum-script-extension-socket.dynamic.compile
+call :cmdX xyo-cc --mode=%ACTION% @build/source/quantum-script-extension-socket.static.compile
+call :cmdX xyo-cc --mode=%ACTION% @build/source/quantum-script-extension-socket.dynamic.compile
--- build/ubuntu.make.sh
+++ build/ubuntu.make.sh
@@ -17,5 +17,5 @@ cmdX(){
 	fi
 }
 
-cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-socket.static.compile
-cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-socket.dynamic.compile
+cmdX xyo-cc --mode=$ACTION @build/source/quantum-script-extension-socket.static.compile
+cmdX xyo-cc --mode=$ACTION @build/source/quantum-script-extension-socket.dynamic.compile
